0281b829689b5eaeb1e40db3c882987a763dd04e
senazz <bagasena.danawira@gmail.com>
2022-12-01 16:05:20 +0700

    hello world

---
+++ index.js
@@ -0,0 +1,6 @@
+var a =document.querySelector("#sen");
+var b=document.querySelector("#googleList");
+var c=document.querySelector("#b");
+a.innerHTML ="sena";
+b.style.color="red";
+c.style.backgroundColor= "yellow";
